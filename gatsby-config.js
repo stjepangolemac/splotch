8df8666d9e91a86ecf7a4830f37b6f3abbf92347
Stjepan Golemac <stjepan@tray.io>
2019-03-06 22:14:35 +0000

    feat(Sitemap): Add sitemap plugin

--- gatsby-config.js
+++ gatsby-config.js
@@ -11,6 +11,7 @@ module.exports = {
   },
   plugins: [
     {
+      'gatsby-plugin-sitemap',
       resolve: `gatsby-source-filesystem`,
       options: {
         path: `${__dirname}/content/blog`,
